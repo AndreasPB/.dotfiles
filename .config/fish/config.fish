f14396316a93207ab2cf39deb97272ea6e64075b
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2021-10-21 10:41:12 +0200

    Crazy aliases that I will forget and not use

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,15 @@ function fish_greeting
         fortune -a | cowsay -f bud-frogs | lolcat
 end
 
+alias str="cd ~/Repo/streamchaser/ && docker-compose exec backend python3 cli.py"
+alias g="git"
+alias ga="git add ."
+alias gco="git commit -m"
+alias gcoa="git commit --amend"
+alias gcoan="git commit --amend --no-edit"
+alias gd="git diff"
+alias gst"git status"
+alias gch="git checkout"
+alias gchm="git checkout master"
+alias gchd="git checkout development"
+alias gpf="git push --force-with-lease origin"
